84907a226dfb36d4b38e7afac2a1f9ce1575fe34
kai <kai@DESKTOP-C8B1BKF>
2019-08-16 13:24:42 +1000

    1

---
+++ Week03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week03.Startup))]
+namespace Week03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
